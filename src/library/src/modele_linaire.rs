6494e0ea0b383001447ab1c279ee57432b2b557f
rayan <rayanstam@icloud.com>
2023-05-04 08:42:15 +0200

    Calcul des poids

--- src/library/src/modele_linaire.rs
+++ src/library/src/modele_linaire.rs
@@ -5,3 +5,10 @@ use rand::{thread_rng, Rng};
 pub extern "C" fn  sigmoid(x: f64) -> f64 {
     1.0 / (1.0 + (-x).exp())
 }
+
+// Tableau pour calculer les poids avec la fonctions sigmoid
+#[no_mangle]
+pub extern "C" fn predict(x: &Array1<f64>, weights: &Array1<f64>) -> f64 {
+    let z = x.dot(weights.slice(s![..-1])) + weights[weights.len() - 1];
+    sigmoid(z)
+}
