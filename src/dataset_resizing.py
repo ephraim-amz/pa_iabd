3abe065078679521a5170f1df93fd589b25659bc
ephraim-amz <ephraimamezian@gmail.com>
2023-05-30 19:38:10 +0200

    Redimensionnement des images pour futur entrainement des modèles + ajout fichier python permettant de faire cela

---
+++ src/dataset_resizing.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import os
+import shutil
+
+# Chemin vers le dossier contenant les images
+dossier_images = "./img/rugby"
+
+# Chemin vers le dossier de sortie pour les images redimensionnées
+dossier_sortie = "./img/rugby_resize"
+
+# Vérifier si le dossier de sortie existe, sinon le créer
+if not os.path.exists(dossier_sortie):
+    os.makedirs(dossier_sortie)
+
+# Parcours des fichiers dans le dossier
+for nom_fichier in os.listdir(dossier_images):
+    chemin_image = os.path.join(dossier_images, nom_fichier)
+
+    # Vérifier la taille de l'image
+    image = Image.open(chemin_image)
+    largeur_image, hauteur_image = image.size
+
+    if largeur_image > 300 or hauteur_image > 300:
+        # Redimensionner l'image si la largeur ou la hauteur est supérieure à 300 pixels
+        largeur_cible = 300
+        facteur_redimensionnement = min(1.0, float(largeur_cible) / largeur_image, float(largeur_cible) / hauteur_image)
+        nouvelle_largeur = int(largeur_image * facteur_redimensionnement)
+        nouvelle_hauteur = int(hauteur_image * facteur_redimensionnement)
+        image_redimensionnee = image.resize((nouvelle_largeur, nouvelle_hauteur), Image.BILINEAR)
+
+        # Créer un nouveau nom de fichier pour l'image redimensionnée
+        nom_fichier_redimensionne = f"redimensionne_{nom_fichier}"
+        chemin_sortie = os.path.join(dossier_sortie, nom_fichier_redimensionne)
+
+        # Convertir l'image en mode RVB avant de la sauvegarder en JPEG
+        image_redimensionnee = image_redimensionnee.convert("RGB")
+
+        # Sauvegarder l'image redimensionnée en JPEG
+        image_redimensionnee.save(chemin_sortie, "JPEG")
+
+    else:
+        # Copier les images d'origine dans le dossier de sortie sans les redimensionner
+        chemin_sortie = os.path.join(dossier_sortie, nom_fichier)
+        shutil.copy2(chemin_image, chemin_sortie)
